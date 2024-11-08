ffb68be052db5b96f8faeed7be5ec7f3fac30817
SilenyProgramator <programatorsileny@gmail.com>
2024-11-08 20:27:29 +0100

    upravil pozdrav
    dsas

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.js</code>.
+            <li>Hello TdA</li>
           </li>
           <li>Zdravím všechny.</li>
         </ol>
